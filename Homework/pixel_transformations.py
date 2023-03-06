0eb2e8aef221e8029e22d85d9cc2fb87c64e542a
vtuptu <vtwop2@gmail.com>
2023-03-06 21:51:50 +0100

    test/update homwork

--- Homework/pixel_transformations.py
+++ Homework/pixel_transformations.py
@@ -16,32 +16,56 @@ import cv2
 import numpy as np
 import os 
 import sys
-
+import matplotlib.pyplot as plt
 
 #set working directory
 PATH = os.environ['PWD']
 
 # Load the images
-img1 = cv2.imread(PATH + '/Homework/images/scene_nocup.jpg', 0)
-img2 = cv2.imread(PATH + '/Homework/images/scene_cup.jpg', 0)
+img1 = cv2.imread(PATH + '/Homework/images/scene_nocup.jpg')
+img2 = cv2.imread(PATH + '/Homework/images/scene_cup.jpg')
 
+img1 = img1[2000:4032, 0:3024]
+img2 = img2[2000:4032, 0:3024]
+
+# import pdb; pdb.set_trace()
 # Convert to 8-bit grayscale images
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+#make image1 more monotone 
+img1 = cv2.addWeighted(img1, 0.2, img1, 0, 0)
+
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-# Take the absolute value of their difference
-img3 = cv2.absdiff(img1, img2)
+# add a small blur
+img1 = cv2.GaussianBlur(img1,(9,9),0)
+#
+img2 = cv2.GaussianBlur(img2,(9,9),0)
 
-# Do a binary threshold of the resulting image
-ret, img4 = cv2.threshold(img3, 50, 255, cv2.THRESH_BINARY)
-
-# Display the results
 cv2.imshow('img1', img1)
 cv2.imshow('img2', img2)
-cv2.imshow('img3', img3)
-cv2.imshow('img4', img4)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+# Take the absolute value of their difference
+img3 = cv2.absdiff(img1, img2 ) 
+
+cv2.imshow('img3', img3)
+
+plt.hist(img3.ravel(),256,[0,256]); plt.show()
+
+
+# threshold 
+ret, img4 = cv2.threshold(img3, 70, 255, cv2.THRESH_BINARY_INV)
+ret, img5 = cv2.threshold(img3, 70, 255, cv2.THRESH_BINARY)
+
+
+cv2.imshow('img4', img4)
+cv2.imshow('img5', img5)
+
+
+plt.hist(img4.ravel(),256,[0,256]); plt.show()
+# Display the histogram of the pixels
+
+
+
 
 
